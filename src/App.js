39861a295d489fc4cf1fec318289d60f960972ee
nvnchandran8 <nvnchandran8@gmail.com>
2020-09-15 00:00:57 +0530

    new Repository

---
+++ src/App.js
@@ -0,0 +1,38 @@
+var input =  require('./App.css');
+
+import React from 'react';
+import logo from './logo.svg';
+
+
+
+
+import styled, { css } from 'styled-components'
+
+
+
+const MyInput = styled.input`
+background-color:pink;
+color:yellow;
+`;
+
+const MainTheme = styled.div`
+width: 600px;
+height: 150px;
+background-color: red;
+& > ${MyInput}:focus {
+  color: green;
+}
+`;
+
+function App() {
+  return (
+    <div>
+      <MainTheme>
+        <MyInput />
+      </MainTheme>
+      <MyInput />
+    </div>
+  );
+}
+
+export default App;
